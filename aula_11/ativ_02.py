4a962b58e1e60c8a97ce70c154a44e6f9579d75b
NaironCarneiro <nsantoscarneiro@gmail.com>
2022-05-25 09:31:37 -0300

    update activity

--- aula_11/ativ_02.py
+++ aula_11/ativ_02.py
@@ -12,25 +12,25 @@ kernelRound = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (121,209))
 kernelRound1 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (119,119))
 
 # Primeiro exemplo
-erosion = cv2.erode(img,kernel,iterations = 1, anchor=(100,100))
+exemploFinal1 = cv2.morphologyEx(img, cv2.MORPH_ERODE, kernel, iterations=1, anchor=(100,100))
 
 # Segundo exemplo
 roundedErode1 = cv2.morphologyEx(img, cv2.MORPH_ERODE,kernelRound)
 roundedErode2 = cv2.morphologyEx(roundedErode1, cv2.MORPH_ERODE, kernelRound)
 roundedDilate = cv2.morphologyEx(roundedErode2, cv2.MORPH_DILATE,kernel1)
-roundedFinal = cv2.morphologyEx(roundedDilate, cv2.MORPH_DILATE, kernelRound)
+exemploFinal2 = cv2.morphologyEx(roundedDilate, cv2.MORPH_DILATE, kernelRound)
 
 # Terceiro exemplo
 roundedDilate = cv2.morphologyEx(img, cv2.MORPH_DILATE, kernelRound1, iterations=2)
-roundedErodeFinal = cv2.morphologyEx(roundedDilate, cv2.MORPH_ERODE,kernelRound1)
+exemploFinal3 = cv2.morphologyEx(roundedDilate, cv2.MORPH_ERODE,kernelRound1)
 
 plt.subplot(221), plt.imshow(img)
 plt.title('Imagem')
-plt.subplot(222), plt.imshow(erosion)
+plt.subplot(222), plt.imshow(exemploFinal1)
 plt.title('Erosion')
-plt.subplot(223), plt.imshow(roundedFinal)
+plt.subplot(223), plt.imshow(exemploFinal2)
 plt.title('border rounded')
-plt.subplot(224), plt.imshow(roundedErodeFinal)
+plt.subplot(224), plt.imshow(exemploFinal3)
 plt.title('dilate border')
 
 
